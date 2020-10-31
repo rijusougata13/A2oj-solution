fd0da6c0c36fcf35397140b7fa5ddb7e42117bcd
aruna2019 <iamaruna.nbp@gmail.com>
2020-10-31 14:12:04 +0530

    soultion added

---
+++ a20j ladders/template.cpp
@@ -0,0 +1,112 @@
+/*
+
+                  ░██████╗░█████╗░██╗░░░██╗░██████╗░░█████╗░████████╗░█████╗░
+                  ██╔════╝██╔══██╗██║░░░██║██╔════╝░██╔══██╗╚══██╔══╝██╔══██╗
+                  ╚█████╗░██║░░██║██║░░░██║██║░░██╗░███████║░░░██║░░░███████║
+                  ░╚═══██╗██║░░██║██║░░░██║██║░░╚██╗██╔══██║░░░██║░░░██╔══██║
+                  ██████╔╝╚█████╔╝╚██████╔╝╚██████╔╝██║░░██║░░░██║░░░██║░░██║
+                  ╚═════╝░░╚════╝░░╚═════╝░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝
+
+                                ███████████████████████████
+                                ███████▀▀▀░░░░░░░▀▀▀███████
+                                ████▀░░░░░░░░░░░░░░░░░▀████
+                                ███│░░░░░░░░░░░░░░░░░░░│███
+                                ██▌│░░░░░░░░░░░░░░░░░░░│▐██
+                                ██░└┐░░░░░░░░░░░░░░░░░┌┘░██
+                                ██░░└┐░░░░░░░░░░░░░░░┌┘░░██
+                                ██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██
+                                ██▌░│██████▌░░░▐██████│░▐██
+                                ███░│▐███▀▀░░▄░░▀▀███▌│░███
+                                ██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██
+                                ██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██
+                                ████▄─┘██▌░░░░░░░▐██└─▄████
+                                █████░░▐█─┬┬┬┬┬┬┬─█▌░░█████
+                                ████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████
+                                █████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████
+                                ███████▄░░░░░░░░░░░▄███████
+                                ██████████▄▄▄▄▄▄▄██████████
+                                ███████████████████████████
+
+*/
+
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#define ll long long
+#define ul unsigned long long
+#define modu 100000000
+#define f(i,j, n) for (ll i = j; i < n; i++)
+#define fo(i, j, n) for (ll i = j; i >=0; i--)
+#define print(x) cout << x << endl
+#define gi(x) scanf("%lld",&x)
+#define gc(x) scanf("%s",&x)
+#define all(o) (o).begin(), (o).end()
+using namespace std;
+
+
+
+void solve(void);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+#endif
+
+    long long t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+
+#ifndef ONLINE_JUDGE
+    cout << "\nTime Elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " sec\n";
+#endif
+    return 0;
+}
+
+struct activity {
+    ll  marks, essay;
+};
+
+bool activity_compare(activity s1, activity s2) {
+    if (s1.essay == s2.essay)
+        return s1.marks < s2.marks;
+    return s1.essay < s2.essay;
+}
+
+ll ncr(ll n, ll k)
+{
+    ll res = 1;
+    if (k > n - k)
+        k = n - k;
+    for (ll i = 0; i < k; ++i) {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
+}
+
+ll max(ll a, ll b)
+{
+    return (a > b) ? a : b;
+}
+ll min(ll a, ll b)
+{
+    return (a < b) ? a : b;
+}
+
+
+
+void solve(void)
+{
+}
